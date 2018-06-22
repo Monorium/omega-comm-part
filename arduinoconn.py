2b812f4cf928d1620103b135c950673f075723fd
Monorium <39909079+Monorium@users.noreply.github.com>
2018-06-22 22:26:41 +0900

    Feature/servo control all (#6)
    16個のサーボをAndroidから操作できるように実装

--- arduinoconn.py
+++ arduinoconn.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 from machine import UART
+import select
 from time import sleep
 from _thread import start_new_thread
 
@@ -15,15 +16,21 @@ class ArduinoConn():
     _STX = 0x02
     _ETX = 0x03
 
+    @staticmethod
+    def _startRecvThread(func, args):
+        print("_startRecvThread")
+        start_new_thread(func, args)
+
     def __init__(self, id=2, baud=115200):
         self._uart = UART(id, baud)
-        self._recvCallback = None
 
     def init(self, txPin=None, rxPin=None):
         self._uart.init(tx=txPin, rx=rxPin)
 
     def startRecvThread(self, callback):
-        start_new_thread(self._recv, callback)
+        print("startRecvThread")
+        ArduinoConn._startRecvThread(self._recv, (callback,))
+        print("startRecvThread end")
 
     def send(self, message):
         buf = bytearray(chr(ArduinoConn._STX)) + bytearray(message.encode()) + bytearray(chr(ArduinoConn._ETX))
@@ -31,17 +38,24 @@ class ArduinoConn():
 
     def _recv(self, callback):
         tele = ArduinoTele()
+        poller = select.poll()
+        poller.register(self._uart, select.POLLIN)
+
         while True:
+            events = poller.poll()
+            print('events =', events)
             while self._uart.any():
                 buf = self._uart.read(1)
+                print(buf)
                 if buf[0] == ArduinoConn._STX:
+                    print("-> STX")
                     tele.clear()
                     tele.isRecving = True
                 elif buf[0] == ArduinoConn._ETX:
-                    callback(tele)
-                    tele.clear()
+                    print("-> ETX")
+                    if tele.isRecving:
+                        print(tele.data)
+                        callback(tele)
+                        tele.clear()
                 elif tele.isRecving:
                     tele.data += buf.decode()
-                sleep(0.001)
-            else:
-                sleep(0.1)
--- omegaWebApp.py
+++ omegaWebApp.py
@@ -4,6 +4,8 @@ from microWebSrv import MicroWebSrv
 from arduinoconn import ArduinoConn
 from servo import JointServo
 
+webSrv = MicroWebSrv(webPath='www/')
+arduino = ArduinoConn()
 servoList = dict()
 
 def _acceptWebSocketCallback(webSocket, httpClient):
@@ -27,7 +29,7 @@ def _recvTextCallback(webSocket, msg):
         except Exception as e:
             print("error :" + e)
         except:
-            print("unknown error :" + e)
+            print("unknown error")
 
 def _recvBinaryCallback(webSocket, data):
     print("WS RECV DATA : %s" % data)
@@ -39,25 +41,26 @@ def _recvArduino(tele):
     global servoList
     print("_recvArduino")
 
-    jsonData = json.loads(tele.data)
-    pin = jsonData["pin"]
-    angle = jsonData["angle"]
+    try:
+        jsonData = json.loads(tele.data)
+        pin = jsonData["pin"]
+        angle = jsonData["angle"]
 
-    if pin in servoList:
-        servo = servoList[pin]
+        servo = None
+        if pin in servoList:
+            servo = servoList[pin]
+        else:
+            servo = JointServo(pin, angle)
+            servoList[pin] = servo
         servo.move(angle)
-    else:
-        servo = JointServo(pin, angle)
-        servoList[pin] = servo
+    except Exception as e:
+        print("error :" + e)
 
 
 # Webサーバ起動
-srv = MicroWebSrv(webPath='www/')
-#srv.MaxWebSocketRecvLen = 256
-srv.WebSocketThreaded		= True
-srv.AcceptWebSocketCallback = _acceptWebSocketCallback
-srv.Start(threaded=True)
+webSrv.WebSocketThreaded = True
+webSrv.AcceptWebSocketCallback = _acceptWebSocketCallback
+webSrv.Start(threaded=True)
 
 # Arduino向けSerial通信開始
-arduino = ArduinoConn()
 arduino.startRecvThread(_recvArduino)
